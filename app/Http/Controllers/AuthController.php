5a548201a971f086e9445abb9cf88f531450088f
rommomm <rommomm@i.ua>
2021-12-17 12:47:22 +0200

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
@@ -40,19 +39,22 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $fields = $request->validate([
-            'email' => 'required|string',
+            'email' => 'required|email',
             'password' => 'required|string'
         ]);
 
         $user = User::where('email', $fields['email'])->first();
 
-        $token = $user->createToken('myToken')->plainTextToken;
 
         if (!$user || !Hash::check($fields['password'], $user->password)) {
             return response([
-                'message' => 'Invalid Login Credentials'
+                'email' => 'Incorrect email',
+                'password' => 'Incorrect password',
+                
             ], 401);
         }
+
+        $token = $user->createToken('myToken')->plainTextToken;
 
         $response = [
             'user' => $user,
@@ -60,6 +62,14 @@ class AuthController extends Controller
         ];
 
         return response($response, 201);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
     }
 
     public function AuthMe($user_name) {
