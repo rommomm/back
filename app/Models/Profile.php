50db4e1b34f1077cec9cd2056852a2703f5fcbef
rommomm <rommomm@i.ua>
2022-01-13 17:41:11 +0200

    stage_5

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,8 +11,8 @@ class Profile extends Model
   protected static function booted()
     {
         static::created(function ($profile) {
-            $profile->update(['profile_photo' => ('http://localhost:8000/uploads/roba/images/avatar/avatar.png'), 
-            'profile_background' => ('http://localhost:8000/uploads/roba/images/avatar/avatar.png')]);
+            $profile->update(['profile_photo' => URL::asset('/uploads/roba/images/avatar/avatar.png'), 
+            'profile_background' => URL::asset('/uploads/roba/images/avatar/avatar.png')]);
         });
     }
 
