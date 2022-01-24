555cc8a7f3c07a4adcace09445075fde7cf720e1
rommomm <rommomm@i.ua>
2022-01-24 16:08:26 +0200

    fixed comments system

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,8 +7,6 @@ use App\Http\Requests\UpdateCommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Post;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 class CommentController extends Controller
 {
     public function index(Post $post)
@@ -28,8 +26,7 @@ class CommentController extends Controller
 
     public function store(CommentRequest $request, Post $post)
     { 
-        $comment = new Comment($request->validated());
-        $comment->user_id = Auth::id();
+        $comment = auth()->user()->comments()->make($request->validated());
         $post->comments()->save($comment);
         return new CommentResource($comment);  
     }
