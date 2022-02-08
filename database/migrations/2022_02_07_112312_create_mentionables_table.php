4e70fdef0fe9e4e566fb962b1d9e458856ba5e56
rommomm <rommomm@i.ua>
2022-02-08 17:06:02 +0200

    added mentions system

--- database/migrations/2022_02_07_112312_create_mentionables_table.php
+++ database/migrations/2022_02_07_112312_create_mentionables_table.php
@@ -14,7 +14,8 @@ class CreateMentionablesTable extends Migration
     public function up()
     {
         Schema::create('mentionables', function (Blueprint $table) {
-            $table->integer("user_id")->constrained("users")->onDelete('cascade');
+            $table->bigIncrements('id');
+            $table->integer("user_id")->constrained()->onDelete('cascade');
             $table->integer("mentionable_id");
             $table->string("mentionable_type");
             $table->timestamps();
